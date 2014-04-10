9a58154b6c6198120e8a45a0eab3df00a92bc16d
i7218850 <i7218850@bournemouth.ac.uk>
2014-04-10 11:50:50 +0100

    Fixed some formatting.

--- Linked/moveEntity.cpp
+++ Linked/moveEntity.cpp
@@ -7,121 +7,101 @@ MoveEntity::MoveEntity()
 
 void MoveEntity::movePlayer(int axis, int direction, FileLoader data)
 {
+	switch(axis)
+	{
+	case false:
+		data.setPlayerX(data.getPlayerX()+direction);
+		break;
+	case true:
+		data.setPlayerY(data.getPlayerY()+direction);					
+		break;
+	}
+	if(collision(data.getGirders(),data.getPlayerX(), data.getPlayerY(), 32, 32, 0, 32, 32, data) == 0)
+	{
+	}
+	else
 	{
 		switch(axis)
 		{
 		case false:
-			data.setPlayerX(data.getPlayerX()+direction);
+			data.setPlayerX(data.getPlayerX()-direction);
 			break;
 		case true:
-			data.setPlayerY(data.getPlayerY()+direction);					
+			data.setPlayerY(data.getPlayerY()-direction);				
 			break;
 		}
-		if(collision(data.getGirders(),data.getPlayerX(), data.getPlayerY(), 32, 32, 0, 32, 32, data) == 0)
-		{
-		}
-		else
+	}
+	if(collision(data.getBombs(),data.getPlayerX(), data.getPlayerY(), 32, 32, 1, 32, 32, data) == 0)//bomb collision check
+	{
+		lightAnim = 0;
+	}
+	else
+	{
+		for (int i = 0; i < data.getBombs(); i++)
 		{
 			switch(axis)
 			{
 			case false:
-				data.setPlayerX(data.getPlayerX()-direction);
+			data.setBombPositionX(i,data.getBombPositionX(i)+direction);
+				switch(direction)
+				{
+				case -1:
+					lightAnim = 96;
+					break;
+				case 1:
+					lightAnim = 128;
+					break;
+				}
 				break;
 			case true:
-				data.setPlayerY(data.getPlayerY()-direction);				
+				data.setBombPositionY(i,data.getBombPositionY(i)+direction);
+				switch(direction)
+				{
+				case -1:
+					lightAnim = 32;
+					break;
+				case 1:
+					lightAnim = 64;
+					break;
+				}
 				break;
 			}
-		}
-		if(collision(data.getBombs(),data.getPlayerX(), data.getPlayerY(), 32, 32, 1, 32, 32, data) == 0)//bomb collision check
-		{
-			lightAnim = 0;
-		}
-		else
-		{
-			for (int i = 0; i < 1; i++)
+			if(collision(data.getGirders(),data.getBombPositionX(i), data.getBombPositionY(i), 32, 32, 0, 32, 32, data) == 0 &&
+				collision(1, data.getBombPositionX(1), data.getBombPositionY(1), 32, 32, 1, 32, 32, data) == 0)//bomb girder collision check and bomb bomb collision check
+			{
+			}
+			else
 			{
 				switch(axis)
 				{
 				case false:
-					data.setBombPositionX(i,data.getBombPositionX(i)+direction);
-					switch(direction)
-					{
-					case -1:
-						lightAnim = 96;
-						break;
-					case 1:
-						lightAnim = 128;
-						break;
-					}
+					data.setBombPositionX(i,data.getBombPositionX(i)-direction);
+					data.setPlayerX(data.getPlayerX()-direction);
 					break;
 				case true:
-					data.setBombPositionY(i,data.getBombPositionY(i)+direction);
-					switch(direction)
-					{
-					case -1:
-						lightAnim = 32;
-						break;
-					case 1:
-						lightAnim = 64;
-						break;
-					}
+					data.setBombPositionY(i,data.getBombPositionY(i)-direction);
+					data.setPlayerY(data.getPlayerY()-direction);
 					break;
-				}
-				if(collision(data.getGirders(),data.getBombPositionX(i), data.getBombPositionY(i), 32, 32, 0, 32, 32, data) == 0 &&
-					collision(1, data.getBombPositionX(1), data.getBombPositionY(1), 32, 32, 1, 32, 32, data) == 0)//bomb girder collision check and bomb bomb collision check
-				{
-				}
-				else
-				{
-
-					switch(axis)
-					{
-					case false:
-						data.setBombPositionX(i,data.getBombPositionX(i)-direction);
-						data.setPlayerX(data.getPlayerX()-direction);
-						break;
-					case true:
-						data.setBombPositionY(i,data.getBombPositionY(i)-direction);
-						data.setPlayerY(data.getPlayerY()-direction);
-						break;
-					}					
-				}
-			/*	if (collision(1, data.getBombPositionX(1), data.getBombPositionY(1), 32, 32, 1, 32, 32, data) == 0)
-				{
-				}
-				else
-				{
-					switch(axis)
-					{
-					case false:
-						data.setBombPositionX(i,data.getBombPositionX(i)-direction);
-						//data.setPlayerX(data.getPlayerX()-direction);
-						break;
-					case true:
-						data.setBombPositionY(i,data.getBombPositionY(i)-direction);
-						//data.setPlayerY(data.getPlayerY()-direction);
-						break;
-					}
-				}*/
+				}					
 			}
 		}
-		bombsXY.resize(data.getBombs());
-		switch(axis)
-		{
-		case false:
-			player.x = data.getPlayerX();
-			player.y = data.getPlayerY();
-			break;
-		case true:
-			player.x = data.getPlayerX();
-			player.y = data.getPlayerY();					
-			break;
-		}
-		for (int i = 0; i < 2; i++)
-		{
-			bombsXY[i].x = data.getBombPositionX(i);
-			bombsXY[i].y = data.getBombPositionY(i);
-		}
+	}
+	bombsXY.resize(data.getBombs());
+	switch(axis)
+	{
+	case false:
+		player.x = data.getPlayerX();
+		player.y = data.getPlayerY();
+		break;
+	case true:
+		player.x = data.getPlayerX();
+		player.y = data.getPlayerY();					
+		break;
+	}
+	for (int i = 0; i < 2; i++)
+	{
+		bombsXY[i].x = data.getBombPositionX(i);
+		bombsXY[i].y = data.getBombPositionY(i);
 	}
 }
 
