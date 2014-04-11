4656ee522382ed024f2ad091272355ef090d677d
i7218850 <i7218850@bournemouth.ac.uk>
2014-04-11 01:57:17 +0100

    Changed the animation on the main menu to be the logo floating up and down. I also fully commented the MainMenu class/header.

--- Linked/mainMenu.cpp
+++ Linked/mainMenu.cpp
@@ -1,15 +1,8 @@
 #include "mainMenu.h"
 
 /*GLOBALS FOR USE WITH TIMERS*/
-int barTopX;
-int barTopAnim;
-int barY;
-std::vector<int> barBotX;
-std::vector<int> barBotY;
-std::vector<int> barBotAnim;
-std::vector<int> directions;
-void pickupAnim(int);
-void moveBotAnim(int i,int dir);
+int logoY;
+int direction;
 /*GLOBALS FOR USE WITH TIMERS*/
 
 MainMenu::MainMenu() //Constructor
@@ -28,103 +21,83 @@ int MainMenu::menuStart()
 	BITMAP *background = load_bitmap( "menuBackground.bmp", NULL );
 	BITMAP *logo = load_bitmap( "linkedLogo.bmp", NULL );
 	BITMAP *sign = load_bitmap( "sign.bmp", NULL );
-	BITMAP *barTop = load_bitmap( "barTop.bmp", NULL );
-	BITMAP *barMiddle = load_bitmap( "barMiddle.bmp", NULL );
-	BITMAP *barBottom = load_bitmap( "barBottom.bmp", NULL );
 	BITMAP *tempSign = load_bitmap( "commingSoon.bmp", NULL );
 	/*END OF LOAD ASSETS*/
 	
+	/*INITIALIZE VARIABLES*/
 	next = -1;
 	buttonPressed = -1;
 	mouseOver;
-	barY = 137;
-	directions.resize(4);
-	directions[0] = 1;//top X Direction
-	directions[1] = 0;//bottom X Direction
-	directions[2] = 0;//bar Y Direction
-	directions[3] = 1;//forwards/reverse
-	barBotX.resize(3);
-	barBotX[0] = 29;
-	barBotX[1] = 573;
-	barBotX[2] = 29;
-	barBotY.resize(3);
-	barBotY[0] = 125;
-	barBotY[1] = 221;
-	barBotY[2] = 317;
-	barBotAnim.resize(3);
-	barBotAnim[0] = 0;
-	barBotAnim[1] = 0;
-	barBotAnim[2] = 0;
-	barTopX = 39;
-	barTopAnim = 0;
-	void animation(void);
-	install_int(animation,10);
+	/*INITIALIZE VARIABLES*/
 
+	/*INITIALIZE VARIABLES FOR TIMERS*/
+	logoY = 60;
+	void animation(void);
+	install_int(animation,50);
+	/*INITIALIZE VARIABLES FOR TIMERS*/
+
+	/*Loop while next is equal to -1**********************************************************/
 	while(next == -1)
 	{	
-		mouseOver = -1;
-		masked_blit( background, buffer, 0, 0, 0, 0, 640, 480 );
-		for (int i = 0; i<3; i++)
-		{
-			masked_blit( barBottom, buffer, barBotAnim[i], 0, barBotX[i], barBotY[i], 38, 38 );
-		}
-		masked_blit( barMiddle, buffer, 0, 0, 0, barY, 640, 12 );
-		masked_blit( barTop, buffer, barTopAnim, 0, barTopX, (barY - 5), 18, 22 );
-		masked_blit( logo, buffer, 0, 0, 70, 80, 500, 127 );
+		mouseOver = -1;//resets mouseOver
+		masked_blit( background, buffer, 0, 0, 0, 0, 640, 480 ); //sets the background to the buffer
+		masked_blit( logo, buffer, 0, 0, 70, logoY, 500, 127 );// set the logo to the buffer
 
 		//check if mouse is over PLAY
 		if (mouse_x >= 147 && mouse_x <= 494 && mouse_y >= 220 && mouse_y <= 270)
 		{
-			masked_blit( sign, buffer, 0, 50, 147, 220, 347, 50 );
+			masked_blit( sign, buffer, 0, 50, 147, 220, 347, 50 );//set the sign to glow green
 			mouseOver = 0;
 		}
 		else
 		{
-			masked_blit( sign, buffer, 0, 0, 147, 220, 347, 50 );
+			masked_blit( sign, buffer, 0, 0, 147, 220, 347, 50 ); //set the sign to be grey
 		}
 
 		//check if mouse is over MAP EDITOR
 		if (mouse_x >= 147 && mouse_x <= 494 && mouse_y >= 280 && mouse_y <= 330)
 		{
-			masked_blit( sign, buffer, 0, 50, 147, 280, 347, 50 );
+			masked_blit( sign, buffer, 0, 50, 147, 280, 347, 50 );//set the sign to glow green
 			mouseOver = 1;
 		}
 		else
 		{
-			masked_blit( sign, buffer, 0, 0, 147, 280, 347, 50 );
+			masked_blit( sign, buffer, 0, 0, 147, 280, 347, 50 ); //set the sign to be grey
 		}
 
 		//check if mouse is over CREDITS
-		if (mouse_x >= 147 && mouse_x <= 494 && mouse_y >= 340 && mouse_y <= 390)
+		if (mouse_x >= 147 && mouse_x <= 494 && mouse_y >= 340 && mouse_y <= 390) 
 		{
-			masked_blit( sign, buffer, 0, 50, 147, 340, 347, 50 );
+			masked_blit( sign, buffer, 0, 50, 147, 340, 347, 50 ); //set the sign to glow green
 			mouseOver = 2;
 		}
 		else
 		{
-			masked_blit( sign, buffer, 0, 0, 147, 340, 347, 50 );
+			masked_blit( sign, buffer, 0, 0, 147, 340, 347, 50 );//set the sign to be grey
 		}
 
 		//check if mouse is over EXIT
 		if (mouse_x >= 147 && mouse_x <= 494 && mouse_y >= 400 && mouse_y <= 450)
 		{
-			masked_blit( sign, buffer, 0, 50, 147, 400, 347, 50 );
+			masked_blit( sign, buffer, 0, 50, 147, 400, 347, 50 );//set the sign to glow green
 			mouseOver = 3;
 		}
 		else
 		{
-			masked_blit( sign, buffer, 0, 0, 147, 400, 347, 50 );
+			masked_blit( sign, buffer, 0, 0, 147, 400, 347, 50 );//set the sign to be grey
 		}
 		
-		
+		/*DISPLAY TEXT*/
 		textprintf_ex(buffer, font, 289, 230, makecol(0,0,0),-1, "PLAY");
 		textprintf_ex(buffer, font, 250, 290, makecol(0,0,0),-1, "MAP EDITOR");
-		masked_blit( tempSign, buffer, 0, 0, 147, 280, 347, 50 );
 		textprintf_ex(buffer, font, 266, 350, makecol(0,0,0),-1, "CREDITS");
 		textprintf_ex(buffer, font, 291, 410, makecol(0,0,0),-1, "EXIT");
 		textprintf_ex(buffer, font, 10, 460, makecol(255,255,255),-1, "(c) Jamie Slowgrove 2014");
+		/*DISPLAY TEXT*/
 
-		show_mouse(buffer);
+		masked_blit( tempSign, buffer, 0, 0, 147, 280, 347, 50 );//display the COMMING SOON sign on top of the map editor button [will be removed when map editor is created]
+
+		show_mouse(buffer);//sets the mouse to display to the screen
 
 		/*DISPLAYING IMAGES TO SCREEN USING A DOUBLE BUFFER*/
 		blit( buffer,screen,0,0,0,0,buffer->w,buffer->h );
@@ -132,52 +105,46 @@ int MainMenu::menuStart()
 
 		switch (mouseOver)
 		{
-		case 0: // PLAY
+		case 0: // if the mouse is over PLAY set button pressed to 2
 			if(mouse_b & 1)
 			{
 				buttonPressed = 2;
 			}
 			break;
-		case 1: // MAP EDITOR
+		case 1: // if the mouse is over MAP EDITOR set button pressed to 3
 			if(mouse_b & 1)
 			{
-				//buttonPressed = 3;
+				//buttonPressed = 3; WILL BE ADDED WHEN MAP EDITOR IS CREATED
 			}
 			break;
-		case 2: //CREDITS
+		case 2: // if the mouse is over CREDITS set button pressed to 4
 			if(mouse_b & 1)
 			{
 				buttonPressed = 4;
 			}
 			break;
-		case 3: //EXIT
+		case 3: // if the mouse is over EXIT set button pressed to 0
 			if(mouse_b & 1)
 			{
 				buttonPressed = 0;
 			}
 			break;
-		default:
+		default://if nothing set buttonPressed to -1
 			buttonPressed = -1;
 			break;
 		}
-		if (buttonPressed != -1 && !(mouse_b & 1))
+		if (buttonPressed != -1 && !(mouse_b & 1)) //if the right mouse button is not equal to -1 then set next to the value of button pressed
 		{
 			next = buttonPressed;
 		}
 	}
+	/*End loop when next is not equal to -1**********************************************************/
 
 	/*DESTROY DATA*/
 	remove_int( animation );
-	directions.resize(0);
-	barBotX.resize(0);
-	barBotY.resize(0);
-	barBotAnim.resize(0);
 	show_mouse(NULL);
 	destroy_font(font);
 	destroy_bitmap( tempSign );
-	destroy_bitmap( barBottom );
-	destroy_bitmap( barMiddle );
-	destroy_bitmap( barTop );
 	destroy_bitmap( sign );
 	destroy_bitmap( logo );
 	destroy_bitmap( background );
@@ -188,92 +155,13 @@ int MainMenu::menuStart()
 
 void animation()
 {
-	if(directions[1] == 0 && barY == 137)
+	if(logoY >= 80)//if the y position of the logo is greater or equal to 80 then set the direction to -1
 	{
-		pickupAnim(0);
+		direction = -1;
 	}
-	else if (directions[1] == 1 && barY == 137)
+	else if(logoY <= 60)//if the y position of the logo is less than or equal to 60 then set the direction to 1
 	{
-		moveBotAnim(0,-1);
+		direction = 1;
 	}
-	else if (directions[0] == -1 && barY <= 233 && barTopX > 39)
-	{
-		barY+=directions[2];
-		barTopX+=directions[0];
-		if (barY == 233)
-		{
-			directions[2] = 0;
-		}
-	}
-	else if(barY == 233 && barTopX == 39 && directions[0] == -1)
-	{
-		directions[2] = 0;
-		directions[0] = 0;
-	}
-	else if(barY == 233 && barTopX == 39 && directions[0] == 0 && directions[1] != 1)
-	{
-		pickupAnim(1);
-	}
-	else if (directions[1] == 1 && barY == 233)
-	{
-		moveBotAnim(1,-1);
-	}
-	else if(barY == 329 && barTopX == 39 && directions[0] == -1)
-	{
-		directions[2] = 0;
-		directions[0] = 0;
-	}
-	else if(barY == 329 && barTopX == 39 && directions[0] == 0 && directions[1] != 1)
-	{
-		pickupAnim(2);
-	}
-	else if (directions[1] == 1 && barY == 329)
-	{
-		moveBotAnim(2,-1);
-	}
-}
-
-void pickupAnim(int i)
-{
-	if (barTopAnim != 36 && barBotAnim[i] != 76)
-	{
-		barTopAnim+=18;
-	}
-	else if (barBotAnim[i] != 76)
-	{
-		barBotAnim[i]+=38;
-	}
-	else if (barTopAnim != 0 && barBotAnim[i] == 76)
-	{
-		barTopAnim-=18;
-	}
-	else
-	{
-		directions[1] = 1;
-	}
-}
-
-void moveBotAnim(int i,int dir)
-{
-	if(barTopX > 660)
-	{
-		directions[0] = dir;
-		directions[1] = 0;
-		if (barY == 329)
-		{
-			directions[2] = dir;
-		}
-		else
-		{
-			directions[2] = -dir;
-		}
-		barY+=directions[2];
-	}
-	else if(barTopX == 39)
-	{
-		directions[0] = -dir;
-		directions[1] = -dir;
-	}
-	barTopX+=directions[0]*directions[3];
-	barBotX[i]+=directions[1]*directions[3];
+	logoY += direction; // the y position of the logo will either increase or decrease depending on whether direction is 1 or -1
 }
